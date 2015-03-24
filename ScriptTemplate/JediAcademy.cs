c1be03a28eea86e31b63985fd79c5c705beaaff2
thegoodest <admin@die-gutesten.net>
2015-03-24 18:28:18 +0100

    Licensing GPL v3

--- ScriptTemplate/JediAcademy.cs
+++ ScriptTemplate/JediAcademy.cs
@@ -2,8 +2,22 @@ using System.Threading.Tasks;
 using System.Windows.Input;
 using VOTCClient.Core.Helpers;
 using VOTCClient.Core.Scripts;
+/*
+    This file is part of VOTC.
 
-// ReSharper disable once CheckNamespace
+    VOTC is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    VOTC is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with VOTC.  If not, see <http://www.gnu.org/licenses/>.
+*/
 namespace JediAcademy
 {
     public class JediAcademy
--- VOTCClient/Pages/StoreUI.xaml.cs
+++ VOTCClient/Pages/StoreUI.xaml.cs
@@ -14,7 +14,22 @@ using VOTCClient.Core;
 using VOTCClient.Core.Extensions;
 using VOTCClient.Core.IO;
 using Path = System.IO.Path;
+/*
+    This file is part of VOTC.
 
+    VOTC is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    VOTC is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with VOTC.  If not, see <http://www.gnu.org/licenses/>.
+*/
 namespace VOTCClient.Pages
 {
     internal class StoreItem
