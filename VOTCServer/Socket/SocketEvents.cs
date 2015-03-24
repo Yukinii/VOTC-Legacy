bd770333d2f294b6d86ba344f787283fef6edb87
thegoodest <admin@die-gutesten.net>
2015-03-24 23:20:51 +0100

    Removed some old code

--- VOTCServer/Socket/SocketEvents.cs
+++
@@ -1,5 +0,0 @@
-﻿namespace VOTCServer.Socket
-{
-    public delegate void NetworkClientConnection(NetworkClient client);
-    public delegate void NetworkClientReceive(NetworkClient client, byte[] packet);
-}
